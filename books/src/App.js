8fcf2fd1d6cf684cf14e01b2cef68b3f305966ae
Jake Lee <leejakejh@gmail.com>
2023-02-20 23:15:59 -0800

    initial commit

---
+++ books/src/App.js
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import BookCreate from './components/BookCreate';
+
+function App() {
+
+  const[books, setBooks] = useState([]);
+
+  const createBook = (title) => {
+    console.log('title!!!: ', title);
+  };
+
+  return (
+    <div>
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
+}
+
+
+export default App;
